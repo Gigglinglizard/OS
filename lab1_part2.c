aea5a3b81a71f62842c777f85102e779b28a2961
eveberg <131174122+eveberg@users.noreply.github.com>
2023-11-15 14:52:21 +0100

    Update lab1_part2.c

--- lab1_part2.c
+++ lab1_part2.c
@@ -11,9 +11,7 @@
 #define MQ_MODE 0666
 
 int main() {
-    mqd_t mqd; 
-
-    mqd = mq_open("/mymq", O_RDONLY | O_CREAT, MQ_MODE, NULL);
+    mqd_t mqd = mq_open("/mymq", O_RDONLY | O_CREAT, MQ_MODE, NULL);
    
     pid_t pid = fork();
 
@@ -27,19 +25,17 @@ int main() {
     if (pid == 0) {
         char *file_path = "./test.txt"; 
 
-        int file_fd = open(file_path, O_RDONLY);
+        int file = open(file_path, O_RDONLY);
 
         int MAX_SIZE = 100000;
         char buf[MAX_SIZE];
-        ssize_t bytes_read = read(file_fd, buf, MAX_SIZE);
+        ssize_t bytes_read = read(file, buf, MAX_SIZE);
         
         char *my_mq = "/mymq";
-        mqd_t mqd_child;
 
-        mqd_child = mq_open(my_mq, O_WRONLY);
+        mqd_t mqd_child = mq_open(my_mq, O_WRONLY);
 
         mq_send(mqd_child, buf, bytes_read, 0);
-        
         mq_close(mqd_child);
 
     // parent process, receives the message and counts
@@ -62,8 +58,6 @@ int main() {
         printf("Message: %s\n", buf);
 
         mq_close(mqd);
-
-        //mq_unlink("/mymq");
     }
 
     return 0;
