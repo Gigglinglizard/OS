be231cc4130df6f93b3356d5ce33831abda7d635
eveberg <131174122+eveberg@users.noreply.github.com>
2023-11-29 16:18:04 +0100

    its lit

--- Assignment3/lab3.c
+++ Assignment3/lab3.c
@@ -8,6 +8,13 @@
 #define PHYSICAL_MEMORY_SIZE 65536
 #define BACKING_STORE_SIZE 65536
 
+
+typedef struct {
+    int frame_number;
+    int page_number;
+} TLBEntry;
+
+
 typedef struct{
     int physical_address;
 } PhysicalMemory;
@@ -17,7 +24,17 @@ typedef struct{
     int frame_number;
 } PageTableEntry;
 
+int tlbHit = 0;
+
 PageTableEntry page_table[256]; 
+TLBEntry tlb[TLB_SIZE];
+int tlb_index = 0;
+
+void initializeTLB() {
+    for (int i = 0; i < TLB_SIZE; ++i) {
+        tlb[i].page_number = -1; // Initialize page numbers to -1 to indicate empty entries
+    }
+}
 
 void initializePageTable() {
     for (int i = 0; i < PAGE_TABLE_SIZE; ++i) {
@@ -30,36 +47,61 @@ void extract_page_and_offset(int number, int *page_number, int *page_offset) {
     *page_offset = number & 0xFF;  // Mask to keep only the lower 8 bits for the page offset 
 }
 
-int main() {
-    PhysicalMemory physicalMemory[256*256]; 
-    initializePageTable();
+void calc_phys(int i, int logical_address){
+    int page, offset;
+    extract_page_and_offset(logical_address, &page, &offset);
+
+    for (int i = 0; i < TLB_SIZE; ++i) {
+        if (tlb[i].page_number == page) {
+            // TLB hit
+            tlbHit++;
+            printf("lol\n");
+            int physical_address = (tlb[i].frame_number * PAGE_SIZE) + offset;
+            printf("Virtual Address: %d, frame number: %d, Physical address: %d\n", logical_address, tlb[i].frame_number, physical_address);
+            return;
+        }
+    }
     
-
-    int numbers[] = {16916, 62493, 30198, 53683};
-    int num_elements = sizeof(numbers) / sizeof(numbers[0]);
-
-    for (int i = 0; i < num_elements; i++) {
-        int page, offset;
-        extract_page_and_offset(numbers[i], &page, &offset);
-
+    //update page table since tlb failed
     page_table[i].valid = 1;
     page_table[i].frame_number = i; 
 
+   
+    
     // Check if the page table entry is valid
     if (page_table[i].valid) {
         // Calculate the physical address using the frame number and offset
         int physical_address = (page_table[i].frame_number * PAGE_SIZE) + offset;
-        printf("Virtual Address: %d, Physical address: %d\n", numbers[i], physical_address);
+
+        tlb[tlb_index].page_number = page;
+        tlb[tlb_index].frame_number = page_table[page].frame_number;
+        tlb_index = (tlb_index + 1) % TLB_SIZE; // Implement FIFO replacement strategy
+
+        printf("Virtual Address: %d, frame number: %d, page number: %d, Physical address: %d\n", logical_address, page_table[i].frame_number, page, physical_address);
+        return;
     } else {
         // Page fault: The page is not currently in memory
         printf("Page fault for virtual address %d\n", i);
-        return -1; // Return an error code for simplicity
+        return;
     }
+}
 
-        
 
 
-        //printf("Number: %d, Page Number: %d, Page Offset: %d\n", numbers[i], page, offset);
+
+
+
+int main() {
+    PhysicalMemory physicalMemory[256][256]; 
+    initializePageTable();
+    initializeTLB();
+    
+
+    int numbers[] = {16916, 62493, 30198, 53683, 40185, 28781, 24462, 48399, 64815, 18295, 12218, 22760, 57982, 27966, 54894, 38929};
+    int num_elements = sizeof(numbers) / sizeof(numbers[0]);
+
+    for (int i = 0; i < num_elements; i++) {
+        calc_phys(i, numbers[i]);
     }
 
     return 0;
